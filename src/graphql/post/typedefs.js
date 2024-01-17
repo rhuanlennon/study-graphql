3e6859f266a9938de2b159f9edd2ee9ece6afd19
rhuan.resende <rhuan.resende@g.globo>
2024-01-17 16:33:33 -0300

    Add post resolvers and type definitions

---
+++ src/graphql/post/typedefs.js
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-server';
+
+export const postTypeDefs = gql`
+  extend type Query {
+    post(id: ID!): Post!
+    posts: [Post!]!
+  }
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    #userId: String!
+    indexRef: Int!
+    createdAt: String!
+    unixTimestamp: String!
+  }
+`;
+
+/*
+{
+"id": "860",
+"title": "Et voluptatem nulla omnis et iusto ullam.",
+"body": "Ad non pariatur. Aut molestias accusamus et inventore sunt voluptates non doloremque illum. Perspiciatis et provident vel et fugiat dolores ut. Quos quibusdam impedit cupiditate quia at eaque quae.",
+"userId": "29",
+"indexRef": 10,
+"createdAt": "2018-08-10T23:41:51.714Z"
+},
+
+*/
