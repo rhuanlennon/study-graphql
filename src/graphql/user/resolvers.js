8af9419defd0929738ba0a0e1074b939e57e1210
rhuan.resende <rhuan.resende@g.globo>
2024-01-17 16:33:50 -0300

    Add user resolvers and type definitions

---
+++ src/graphql/user/resolvers.js
@@ -0,0 +1,17 @@
+const users = async (_, __, { getUsers }) => {
+  const users = await getUsers();
+  return users.json(); // converte para json
+};
+
+const user = async (_, { id }, { getUsers }) => {
+  const response = await getUsers('/' + id);
+  const user = await response.json();
+  return user;
+};
+
+export const userResolvers = {
+  Query: {
+    user,
+    users,
+  },
+};
