d23224202241cece31f893a7a130e69000d72574
rhuan.resende <rhuan.resende@g.globo>
2024-01-20 00:44:09 -0300

    Add PostNotFoundError union type to postTypeDefs

--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -2,9 +2,16 @@ import { gql } from 'apollo-server';
 
 export const postTypeDefs = gql`
   extend type Query {
-    post(id: ID!): Post!
+    post(id: ID!): PostResult!
     posts(input: ApiFiltersInputs): [Post!]!
   }
+  union PostResult = PostNotFoundError | Post
+
+  type PostNotFoundError {
+    statusCode: Int!
+    message: String!
+  }
+
   type Post {
     id: ID!
     title: String!
@@ -15,15 +22,3 @@ export const postTypeDefs = gql`
     unixTimestamp: String!
   }
 `;
-
-/*
-{
-"id": "860",
-"title": "Et voluptatem nulla omnis et iusto ullam.",
-"body": "Ad non pariatur. Aut molestias accusamus et inventore sunt voluptates non doloremque illum. Perspiciatis et provident vel et fugiat dolores ut. Quos quibusdam impedit cupiditate quia at eaque quae.",
-"userId": "29",
-"indexRef": 10,
-"createdAt": "2018-08-10T23:41:51.714Z"
-},
-
-*/
