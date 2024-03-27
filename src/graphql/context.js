e745e4f2d02135a72051668f68b9bbfc3f750b87
rhuan.resende <rhuan.resende@g.globo>
2024-03-26 22:29:48 -0300

    Remove unnecessary code in graphql context.js

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -1,16 +1,3 @@
-import fetch from 'node-fetch';
-import { getUsers } from './user/utils';
-import { getPosts } from './post/utils';
-import { makeUserDataLoader } from './user/dataloaders';
-
-// const API_URL = process.env.API_URL;
-const _getUsers = getUsers(fetch);
-const _getPosts = getPosts(fetch);
-
 export const context = () => {
-  return {
-    userDataLoader: makeUserDataLoader(_getUsers),
-    getUsers: _getUsers,
-    getPosts: _getPosts,
-  };
+  return {};
 };
